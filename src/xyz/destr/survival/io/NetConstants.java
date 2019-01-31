c058545836d5d3a21e8fb73fc52c28d578b160a6
Destrovi4 <Destrovi4@gmail.com>
2019-01-31 23:48:34 +0400

    Обновление версии протокола

--- src/xyz/destr/survival/io/NetConstants.java
+++ src/xyz/destr/survival/io/NetConstants.java
@@ -3,6 +3,6 @@ package xyz.destr.survival.io;
 public class NetConstants {
 	
 	public static final int DEFAULT_PORT = 8090;
-	public static final String HANDSHAKE_WORD = "@SURVIVAL@0.0.1@27.01.2019";
+	public static final String HANDSHAKE_WORD = "@SURVIVAL@0.0.1@31.01.2019";
 	
 }
